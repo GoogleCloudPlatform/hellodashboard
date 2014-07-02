13723e43e7dba1811446ae895babb3639ff67743
feczo <szabolcs.feczak@gmail.com>
2014-07-02 22:20:11 +0100

    Update main.py
    defining regions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# [START imports]
 import os
 import webapp2
 import logging
@@ -5,7 +6,9 @@ from oauth2client.appengine import oauth2decorator_from_clientsecrets
 from googleapiclient.discovery import build
 from googleapiclient import errors
 from google.appengine.api import memcache
+# [END imports]
 
+# [START decorator]
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 SCOPES = [
     'email',
@@ -15,9 +18,10 @@ decorator = oauth2decorator_from_clientsecrets(
     filename=CLIENT_SECRETS,
     scope=SCOPES,
     cache=memcache)
-
+# [END decorator]
 
 class MainPage(webapp2.RequestHandler):
+    # [START given_name]
     def _get_given_name(self):
         """Send a request to the UserInfo API to retrieve the user's given name.
 
@@ -38,11 +42,14 @@ class MainPage(webapp2.RequestHandler):
             return user_info.get('given_name')
         else:
             raise Exception('No given name found!')
-
+    # [END given_name]
+    
+    # [START decorated_get]
     @decorator.oauth_required
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello %s!' % self._get_given_name())
+    # [END decorated_get]
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
